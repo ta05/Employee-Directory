de99fe4d4f0d3215f4ff5be0c3767a28ec7eb529
Tolu Alimi <tolualimi05@gmail.com>
2020-08-26 10:08:11 -0500

    Added Title component. Added an employee and header state variable to the App class. Wrote the props passed down to TableRow.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 import React, {Component} from 'react';
-import { Table, TableBody, TableData, TableHead, TableHeader, TableRow, Wrapper } from "./components";
+import { Table, TableBody, TableData, TableHead, TableHeader, TableRow, Title, Wrapper } from "./components";
+import employees from "./employees.json";
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    employees,
+    header: {
+      id: "ID",
+      name: "Name",
+      role: "Role",
+      department: "Department",
+      salary: "Salary"
+    }
+  };
+
   render() {
     return (
       <Wrapper>
+        <Title>Employee Table</Title>
         <Table>
           <TableHead>
-            <TableRow>
-
-            </TableRow>
+            <TableRow employee={this.state.header}/>
           </TableHead>
 
           <TableBody>
-
+            {this.state.employees.map(employee => (
+              <TableRow
+                key={employee.id}
+                employee={employee}
+              />
+            ))}
           </TableBody>
         </Table>
       </Wrapper>
